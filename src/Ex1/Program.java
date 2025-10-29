64393e4d1130c0224b3d913ceefb898e0cc879e8
Bruno Fontes <brnfreis@gmail.com>
2025-10-29 17:41:44 -0300

    Versão 3 - Object

--- src/Ex1/Program.java
+++ src/Ex1/Program.java
@@ -6,9 +6,9 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        PrintServiceString ps = new PrintServiceString();
+        PrintService ps = new PrintService();
 
-        System.out.println("How many values? ");
+        System.out.print("How many values? ");
         int n = sc.nextInt();
 
         for (int i = 0; i< n; i++){
