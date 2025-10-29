ab65ba4d1d16f970007b76864722f4859248a368
Bruno Fontes <brnfreis@gmail.com>
2025-10-29 17:33:33 -0300

    Initial commit

---
+++ src/Ex1/Program.java
@@ -0,0 +1,24 @@
+package Ex1;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        PrintService ps = new PrintService();
+
+        System.out.println("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i< n; i++){
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+    }
+
+}
