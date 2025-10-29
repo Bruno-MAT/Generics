077e6d0b3632a4ed3cf0bfdf4e2529c5adfa844e
Bruno Fontes <brnfreis@gmail.com>
2025-10-29 17:36:49 -0300

    Versão 2 - String

--- src/Ex1/Program.java
+++ src/Ex1/Program.java
@@ -6,13 +6,13 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        PrintService ps = new PrintService();
+        PrintServiceString ps = new PrintServiceString();
 
         System.out.println("How many values? ");
         int n = sc.nextInt();
 
         for (int i = 0; i< n; i++){
-            int value = sc.nextInt();
+            String value = sc.next();
             ps.addValue(value);
         }
         ps.print();
